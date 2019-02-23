13d987a60d824336f6558d9baf168fd332b2feb8
PurpleEdge2214 <github@comformark.com.au>
2019-02-23 15:55:12 +1100

    Side Menu Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
-
+ 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
---
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss'],
+})
+export class SidemenuComponent implements OnInit {
+
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    }
+  ];
+
+
+  constructor(
+
+  ) { }
+
+  ngOnInit() { }
+
+}
